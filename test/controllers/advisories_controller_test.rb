494743d5bcb5a08b287b73bdeb47c96a2ef43b88
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-28 15:20:44 -0400

    Change references to FactoryBot

--- test/controllers/advisories_controller_test.rb
+++ test/controllers/advisories_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class AdvisoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    FactoryGirl.create_list(:rubymem_advisory, 5)
+    FactoryBot.create_list(:rubymem_advisory, 5)
     get advisories_path
     assert_response :success
   end
 
   test "should get show" do
-    advisory = FactoryGirl.create(:rubymem_advisory)
+    advisory = FactoryBot.create(:rubymem_advisory)
     get advisory_path(advisory)
     assert_response :success
   end
--- test/factories/rubysec_advisories.rb
+++ test/factories/rubysec_advisories.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   sequence :gem do |n|
     "gem_number#{n}"
   end
@@ -7,7 +7,7 @@ FactoryGirl.define do
     identifier { gem }
     gem
     date { rand(1..99).weeks.ago }
-    title "this is a title"
+    title { "this is a title" }
     patched_versions { ["> 1.2.3", "~> 3.2"] }
     unaffected_versions { [">= 1.2.4", "~> 2.2"] }
     updated_at { rand(1..99).weeks.ago }
